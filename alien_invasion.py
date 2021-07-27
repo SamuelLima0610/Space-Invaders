cd8cebb5791f918fbb0529d2585f5a5d83d5c50c
Samuel Vitorio Lima <34277130+SamuelLima0610@users.noreply.github.com>
2021-07-27 11:39:26 -0300

    Add the Bullet resources to Ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """ Overeall class to manage game assets and behavior """
@@ -13,13 +14,16 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """ Start the main loop for the game"""
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullets();
             self._update_screen()
+
     
     def _check_events(self):
         """Respond to keypresses and mouse events."""
@@ -37,6 +41,8 @@ class AlienInvasion:
         #Redraw the screen during each pass through the loop.
         self.screen.fill(self.settings.bg_color)        
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         # Make the most recently drawn screen visible
         pygame.display.flip()
 
@@ -47,14 +53,33 @@ class AlienInvasion:
         if event.key == pygame.K_LEFT:
             self.ship.moving_left = True 
         if event.key == pygame.K_q:
-            sys.exit()    
+            sys.exit()
+        if event.key == pygame.K_SPACE:
+            self._fire_bullet()        
 
     def _check_events_keyup(self,event):
         """Respond to key releases."""
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
         if event.key == pygame.K_LEFT:
-            self.ship.moving_left = False 
+            self.ship.moving_left = False
+
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group."""
+        if len(self.bullets) < self.settings.bullets_allowed: 
+            new_bulltet = Bullet(self)
+            self.bullets.add(new_bulltet)
+
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets."""
+        # Update bullet positions.
+        self.bullets.update()
+
+        # Get rid of bullets that have disappeared.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                 self.bullets.remove(bullet)        
+
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
